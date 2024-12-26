5ef52607d97a14f8aeadaecf26674e7d8de41399
Xuannn28 <jgoh0043@student.monash.edu>
2024-12-26 16:34:47 +0800

    create contact, footer sections

---
+++ src/components/Contact.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import { motion } from "framer-motion";
+
+const Contact = () => {
+const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");  // show Sending... when form is submitted
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "254e8c1d-b678-4946-919f-7d56dd2d5cd7");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("");  // clear the result when form is submitted successfully
+      alert("Form Submitted Successfully");  // show alert on window
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      alert(data.message)
+      setResult("");
+    }
+  };
+
+  return (
+    <div className='w-full mx-auto' id='Contact'>
+        <motion.div initial={{ opacity: 0, y:100 }} whileInView={{ opacity: 1, y:0}} transition={{ duration: 1 }} viewport={{ once:true }}
+        className='z-10 relative grid grid-cols-1 md:grid-cols-2' >
+            
+            {/* header */}
+            <div className='flex flex-col justify-center items-center text-center p-6 md:p-15'
+            style={{
+                backgroundImage: `url('/contact_background.jpg')`, 
+                backgroundSize: 'cover', 
+                backgroundPosition: 'center', 
+              }} >
+                <h3 className='text-white text-3xl font-bold py-4 px-6 pt-20 sm:pt-0 md:px-20 lg:px-32 sm:text-4xl inline-block'>
+                        Contact Us
+                </h3>
+                <p className='text-lg text-gray-300 px-6 pt-4 md:px-20 lg:px-32'>
+                    We'd love to hear from you! Fill out the form and our team will get back
+                    to you as soon as possible.
+                </p>
+            </div>
+
+            {/* form */}
+            <form onSubmit={onSubmit} className='gap-4 flex flex-col w-[70%] m-auto p-6 md:p-15'>
+                    <div className='w-full text-left '>
+                        Your Name
+                        <input type='text' placeholder='Your Name' name='Name' required
+                        className='w-full border border-gray-400 rounded py-3 px-4 mt-2'/>
+                    </div>
+
+                    <div className='w-full text-left py-4'>
+                        Your Email
+                        <input type='email' placeholder='email@address.com' name='Email' required
+                        className='w-full border border-gray-400 rounded py-3 px-4 mt-2'/>
+                    </div>
+
+                    <div className='w-full text-left py-4'>
+                        Your Message
+                        <textarea className='w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none' placeholder='Message' name='Message' required></textarea>
+                    </div>
+                    
+                    <button className='w-full text-center py-4 bg-slate-300 border border-slate-500 px-8 rounded-full hover:bg-slate-500 hover:text-slate-100'>
+                            {result ? result : "Send Message"}  
+                            {/* if result is not Sending..., display send message */}
+                        
+                    </button>
+            </form>
+
+        </motion.div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Footer.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='w-full bg-gray-800 text-white mx-auto'>
+        <div className='z-10 relative grid grid-cols-1 lg:grid-cols-3 p-10 md:p-15 gap-5'>
+
+            <div>
+                <div className='flex flex-row gap-5'>
+                    <a href='#Header' className='cursor-pointer'>
+                        <img src={assets.logo} alt='logo' width={60} height={60} className='object-contain'/>
+                    </a>
+                    <h3 className='mt-3 text-white font-bold font-sans'>
+                        Malaysia Adventure
+                    </h3>
+                </div>
+
+                <p className='text-gray-300 mt-5 max-w-sm'>
+                    We are a team of experienced tour guides who are passionate about adventure and travel. 
+                    Our goal is to make your trip as memorable as possible.
+                </p>
+            </div>
+
+            <div>
+                <h3 className='text-white font-bold font-sans mt-3'>
+                    Contact Us
+                </h3>
+
+                <ul className='text-gray-300 mt-5 max-w-sm'>
+                    <li>Email: malaysiaadventure@gmail.com</li>
+                    <li>Phone: +60123456789</li>
+                    <li>Location: Kuala Lumpur, Malaysia</li>
+                </ul>
+
+            </div>
+
+            <div>
+                <h3 className='text-white font-bold font-sans mt-3'>
+                    Social Share
+                </h3>
+
+                <div className='flex flex-row gap-5 mt-5'>
+                    <a href='#Facebook' className='cursor-pointer'>
+                        <img src={assets.facebook} width={40} className='rounded-full object-contain'/>
+                    </a>
+
+                    <a href='#Instagram' className='cursor-pointer'>
+                        <img src={assets.instagram} width={40} className='rounded-full object-contain'/>
+                    </a>
+
+                    <a href='#Twitter' className='cursor-pointer'>
+                        <img src={assets.twitter} width={40} className='rounded-full object-contain'/>
+                    </a>
+
+                    <a href='#Youtube' className='cursor-pointer'>
+                        <img src={assets.youtube} width={40} className='rounded-full object-contain'/>
+                    </a>
+
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <div className='py-2 mt-3 text-center text-gray-300'>
+            Copyright 2024 @ Malaysia Adventure. All Right Reserved.
+        </div>
+    </div>
+  )
+}
+
+export default Footer
