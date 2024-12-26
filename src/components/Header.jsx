06fea0e408511d766bc2a48661441dfead7abf6f
Xuannn28 <jgoh0043@student.monash.edu>
2024-12-26 16:33:50 +0800

    create header and about section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { motion } from "framer-motion";
 
 const Header = () => {
   return (
-    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden'>
-        Header
+    <div className='min-h-screen bg-cover bg-center flex items-center w-full overflow-hidden relative' 
+    style={{backgroundImage: "url('/header_img.jpg')"}} 
+    id='Header'>
+
+      {/* darker background */}
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>  
+
+      <motion.div initial={{ opacity: 0, y:100 }} whileInView={{ opacity: 1, y:0}} transition={{ duration: 1 }} viewport={{ once:true }}
+      className='container mx-auto text-white text-center z-10 py-4 px-6 pt-20 sm:pt-0 md:px-20 lg:px-32'>
+        
+        <h2 className='text-3xl font-bold sm:text-4xl inline-block'>
+          Discover Malaysia: A Land of Endless Adventures
+        </h2>
+        
+        <p className='text-lg mt-4 mx-6'>
+          Explore Malaysia's stunning beaches, vibrant cities, and rich culture. Your adventure starts here!
+        </p>
+
+        <div className='flex flex-col mt-16 sm:flex-row gap-4 justify-center items-center'>
+          <a href='#Destinations' className='border border-white px-8 py-3 rounded-full hover:bg-slate-100 hover:text-slate-700'>Destinations</a>
+          <a href='#Contact' className='border border-white inset-0 bg-black bg-opacity-50 px-8 py-3 rounded-full  hover:bg-slate-100 hover:text-slate-700'>Contact Us</a>
+        </div>
+      
+      </motion.div>
     </div>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 
 const Nav = () => {
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+    // prevent website from scrolling when shoeMobileMenu is true
+    useEffect(() => {
+      if(showMobileMenu){
+        document.body.style.overflow = 'hidden'
+      }else {
+        document.body.style.overflow = 'auto'
+      }
+      return () => {
+        document.body.style.overflow = 'auto'
+      }
+    }, [showMobileMenu])
+    
   return (
-    <div className='absolute top-0 left-0 w-full z-10'>
+    <div className='absolute top-0 left-0 w-full z-20'>
         <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
+
             <img src={assets.logo} alt='logo' width={100} height={100} className='object-contain'/>
-            <ul className='hidden md:flex gap-10 text-black'>
+
+            <ul className='hidden md:flex gap-8 text-black font-semibold font-sans'>
                 <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>About</a>
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Projects</a>
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+                <a href="#About" className='cursor-pointer hover:text-gray-400'>About</a>
+                <a href="#Destinations" className='cursor-pointer hover:text-gray-400'>Destinations</a>
+                <a href="#Testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
             </ul>
-            <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
+            
+            <button className='hidden md:block bg-white px-6 py-2 rounded-full'>
+              Sign Up
+            </button>
+
+            <img onClick={() => setShowMobileMenu(true)} src={assets.hamburger} alt='menu hamburger' width={30} height={30} className='md:hidden cursor-pointer'/>
+        </div> 
+
+        {/* mobile menu */}
+        <div className={`md:hidden ${showMobileMenu ? 'fixed w-full' : 'h-0 w-0'} right-0 top-0 bottom-0 overflow-hidden bg-white bg-opacity-80 transition-all`}>
+          <div className='flex justify-end p-5 cursor-pointer'>
+            <img onClick={() => setShowMobileMenu(false)} src={assets.cross} alt='cross icon' width={25} height={25}/>
+          </div>
+
+          <ul className='flex flex-col items-center gap-1 mt-5 px-5 font-semibold font-sant'>
+            <a onClick={() => setShowMobileMenu(false)} href='#Header' className='px-4 py-2 rounded-full inline-block'>Home</a>
+            <a onClick={() => setShowMobileMenu(false)} href='#About' className='px-4 py-2 rounded-full inline-block'>About</a>
+            <a onClick={() => setShowMobileMenu(false)} href='#Destinations' className='px-4 py-2 rounded-full inline-block'>Destinations</a>
+            <a onClick={() => setShowMobileMenu(false)} href='#Testimonials' className='px-4 py-2 rounded-full inline-block'>Testimonials</a>
+          </ul>
         </div>
     </div>
   )
