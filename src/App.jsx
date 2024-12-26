ab1aa4301105d2a6c517423084eddd482e9460de
Xuannn28 <jgoh0043@student.monash.edu>
2024-12-26 16:35:08 +0800

    add smooth transition for scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import Nav from './components/Nav'
+import Header from './components/Header'
+import About from './components/About'
+import Destinations from './components/Destinations'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>
+    <div className='w-full overflow-hidden'>
       <Nav/>
+      <Header/>
+      <About/>
+      <Destinations/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
     </div>
+
   )
 }
 
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,45 @@
 import logo from './logo.jpg';
+import hamburger from './hamburger.png';
+import cross from './cross.png';
+import travel_icon from './travel_icon.jpg';
+import benefit_icon from './benefits.png';
+import about_us from './about_us.jpg';
+import awards from './awards.jpg';
+import twin_towers from './twin_tower.png';
+import kinabalu from './kinabalu.jpg';
+import island from './island.jpg';
+import food from './food.jpeg';
+import person1 from './person1.jpg';
+import person2 from './person2.jpg';
+import person3 from './person3.png';
+import star from './star.png';
+import contact_background from './contact_background.jpg';
+import facebook from './facebook.png';
+import instagram from './instagram.jpg';
+import twitter from './twitter.jpg';
+import youtube from './youtube.jpg';
 
 export const assets = {
-    logo
+    logo,
+    hamburger,
+    cross,
+    travel_icon,
+    benefit_icon,
+    about_us,
+    awards,
+    twin_towers,
+    kinabalu,
+    island,
+    food,
+    person1,
+    person2,
+    person3,
+    star,
+    contact_background,
+    facebook,
+    instagram,
+    twitter,
+    youtube
+
 };
 
