af9b7f599e8f95fb3bf5a3b1208198129445bd3c
Xuannn28 <jgoh0043@student.monash.edu>
2025-01-21 17:29:27 +0800

    create plan journey frontend

---
+++ src/assets/planJourneyData.js
@@ -0,0 +1,102 @@
+
+
+export const data = [
+    {
+      state: 'Kuala Lumpur',
+      attractions: [
+        'Petronas Twin Towers',
+        'Batu Caves',
+        'KL Tower',
+        'Dataran Merdeka',
+        'Central Market',
+      ],
+      foods: [
+        'Nasi Lemak',
+        'Char Kuey Teow',
+        'Roti Canai',
+        'Hainanese Chicken Rice',
+        'Satay Kajang',
+      ],
+      hotels: [
+        'Mandarin Oriental Kuala Lumpur',
+        'The Majestic Hotel Kuala Lumpur',
+        'Traders Hotel Kuala Lumpur',
+        'Aloft Kuala Lumpur Sentral',
+        'Hotel Stripes Kuala Lumpur',
+      ],
+    },
+    {
+      state: 'Penang',
+      attractions: [
+        'George Town (UNESCO World Heritage Site)',
+        'Penang Hill',
+        'Kek Lok Si Temple',
+        'Penang National Park',
+        'Cheong Fatt Tze Mansion (Blue Mansion)',
+      ],
+      foods: [
+        'Penang Laksa',
+        'Char Kuey Teow',
+        'Penang Hokkien Mee',
+        'Rojak',
+        'Nasi Kandar',
+      ],
+      hotels: [
+        'Eastern & Oriental Hotel',
+        'The Prestige Hotel Penang',
+        'Bayview Beach Resort Penang',
+        'The Edison George Town',
+        'Hard Rock Hotel Penang',
+      ],
+    },
+    {
+      state: 'Sarawak',
+      attractions: [
+        'Gunung Mulu National Park (UNESCO World Heritage Site)',
+        'Kuching Waterfront',
+        'Bako National Park',
+        'Semenggoh Wildlife Centre (Orangutans)',
+        'Sarawak Cultural Village',
+      ],
+      foods: [
+        'Sarawak Laksa',
+        'Kolo Mee',
+        'Manok Pansoh (Chicken in Bamboo)',
+        'Midin (Wild Fern)',
+        'Terung Dayak (Dayak Brinjal)',
+      ],
+      hotels: [
+        'Hilton Kuching',
+        'The Waterfront Hotel Kuching',
+        'Damai Beach Resort',
+        'Pullman Kuching',
+        'Borneo Highlands Resort',
+      ],
+    },
+    {
+      state: 'Sabah',
+      attractions: [
+        'Mount Kinabalu',
+        'Tunku Abdul Rahman Marine Park',
+        'Sepilok Orangutan Rehabilitation Centre',
+        'Poring Hot Springs',
+        'Sabah Tea Garden',
+      ],
+      foods: [
+        'Tuaran Mee',
+        'Hinava (Fish Salad)',
+        'Bosou (Fermented Rice and Meat)',
+        'Sang Nyuk Mee (Pork Noodles)',
+        'Latok (Sea Grapes)',
+      ],
+      hotels: [
+        'Shangri-La Rasa Ria Resort',
+        'Le Méridien Kota Kinabalu',
+        'The Magellan Sutera Resort',
+        'Gaya Island Resort',
+        'Grandis Hotels and Resorts',
+      ],
+    },
+  ];
+
+  
--- src/components/PlanJourney.jsx
+++ src/components/PlanJourney.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
-import L from 'leaflet';
+import { Link } from 'react-router-dom';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { data } from '../assets/planJourneyData'
 
-// if user click on any of the places shown in recommendation section, replace it as destination and the older one into depart.
-// if user click save, it will save form data into backend database.
 
 const PlanJourney = () => {
 
@@ -26,6 +26,9 @@ const PlanJourney = () => {
 
     // state to track distance
     const [ distance, setDistance ] = useState(0);
+
+    // state to track city
+    const [ state, setState ] = useState('');
 
     // function to calculate distance via Haversine formula
     const calcDistance = (latDep, latDest, lonDep, lonDest) => {
@@ -66,18 +69,27 @@ const PlanJourney = () => {
             if (dataDestination && dataDestination.length > 0 && dataDeparture && dataDeparture.length > 0) {
                 console.log("Departure: ",dataDeparture)
                 console.log("Destination: ",dataDestination)
-
-                // use first result of data (result returned are sorted by relevance by default)
-                const { lat:latDep, lon:lonDep } = dataDeparture[0];
-                const { lat:latDest, lon:lonDest } = dataDestination[0];
                 
-                // set lat, lon of destination as marker position
-                setMarkerPosition([parseFloat(latDest), parseFloat(lonDest)]);
+                // validation check: destination is within Malaysia
+                const { display_name } = dataDestination[0]
+                if (display_name.includes('Malaysia')){
+                    // use first result of data (result returned are sorted by relevance by default)
+                    const { lat:latDep, lon:lonDep } = dataDeparture[0];
+                    const { lat:latDest, lon:lonDest } = dataDestination[0];
+                    
+                    // set lat, lon of destination as marker position
+                    setMarkerPosition([parseFloat(latDest), parseFloat(lonDest)]);
 
-                // calculate distance between departure and destination
-                const dist = calcDistance(latDep, latDest, lonDep, lonDest);
-                setDistance(dist.toFixed(2));
+                    // calculate distance between departure and destination
+                    const dist = calcDistance(latDep, latDest, lonDep, lonDest);
+                    setDistance(dist.toFixed(2));
 
+                    // find city of destination
+                    getState(latDest, lonDest);
+
+                }else {
+                    alert('Destination is not in Malaysia. Please try again.')
+                }
             }else {
                 alert('Depature/destination not found.')
             }
@@ -87,28 +99,116 @@ const PlanJourney = () => {
         }
     }   
 
+    const getState = async (lat, lon) => {
+        // reverse geocoding
+        try{
+            // fetch destination city based on coodinates
+            const resState = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json&zoom=5&accept-language=en`);
+
+            // convert to json format
+            const data = await resState.json();
+
+            if (data && data.address){
+                const state = data.address.state;
+                setState(state);
+            } else {
+                alert('City of the destination is not found. Please be more specific.')
+            }
+
+        }catch (error) {
+            console.log(`getCity error: ${error}`);
+            alert("Failed to fetch city location. Please try again.")
+        }
+    }
+
+    /**
+     * when destination change, fly marker position and zoom in the map.
+     * @param {*} param0 
+     * @returns 
+     */
+    const MapZoomer = ({position}) => {
+        const map = useMap();
+
+        if (position) {
+            map.flyTo(position, 15)
+        }
+
+        return null;
+    }
+
     // handler function to update selected button
     const handleButton = (selectedButton) => {
         setCurrentButton(selectedButton);
     };
 
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0
+        })
+    }
+
     // content of respective button
     const content = () => {
-        switch (currentButton) {
-            case 'Attractions':
-                return <div className='w-full bg-slate-200'>
-                            <h3>Attraction</h3>
-                        </div>;
-            case 'Foods':
-                return <div className='w-full bg-slate-200'>
-                            <h3>Foods</h3>
-                        </div>;
-            default:
-                return <div className='w-full bg-slate-200'>
-                            <h3>Hotels</h3>
-                        </div>;
+        // find the place with matching state
+        const recommendation = data.find((place) => place.state === state);
+
+        // if state is included, show all content, otherwise show 'no recommendation found'
+        if (recommendation) {
+            switch (currentButton) {
+                case 'Attractions':
+                    return (
+                        <div>
+                            <h3 className='font-semibold text-lg'>
+                                Recommended attraction in {state}
+                            </h3>
+
+                            <ul className='list-disc pl-6'>
+                            {recommendation.attractions.map((attraction, index) => (
+                                <li key={index}> {attraction}</li>
+                            ))}
+                            </ul>
+                        </div>
+                    );
+                case 'Foods':
+                    return (
+                        <div>
+                            <h3 className='font-semibold text-lg'>
+                                Recommended foods in {state}
+                            </h3>
+                            
+                            <ul className='list-disc pl-6'>
+                            {recommendation.foods.map((attraction, index) => (
+                                <li key={index}> {attraction}</li>
+                            ))}
+                            </ul>
+                        </div>
+                    );
+                case 'Hotels':
+                    return (
+                        <div>
+                            <h3 className='font-semibold text-lg'>
+                                Recommended hotels in {state}
+                            </h3>
+                            
+                            <ul className='list-disc pl-6'>
+                            {recommendation.hotels.map((attraction, index) => (
+                                <li key={index}> {attraction}</li>
+                            ))}
+                            </ul>
+                        </div>
+                    );
+            }
+        } else {
+            return (
+                <div>
+                    Currently no recommendation for this state.
+                    (Available: Kuala Lumpur, Penang, Sarawak, Sabah)
+                </div>
+            )
+
         }
-    };
+    }
+
 
   return (
     <div className='min-h-screen w-full m-auto bg-cover bg-center overflow-hidden relative pt-[6rem]' 
@@ -119,9 +219,9 @@ const PlanJourney = () => {
         <div className='z-0 relative'>
 
             <div className='flex flex-row gap-5 text-3xl text-white font-bold py-4 px-6 sm:pt-0 md:px-20 lg:px-32 sm:text-4xl'>
-                <a href='#Header' className='cursor-pointer'>
+                <Link to='/' onClick={scrollToTop} className='cursor-pointer'>
                     <img src={assets.logo} alt='logo' width={100} height={100} className='object-contain'/>
-                </a>
+                </Link>
                 <h3 className='mt-5'>
                     Plan Your Journey
                 </h3>
@@ -130,7 +230,7 @@ const PlanJourney = () => {
             
             <div className='grid grid-cols-1 md:grid-cols-2 p-6 mb-5'>
                 {/* planning journey form */}
-                <div className='w-1/2 lg:h-[550px] mx-auto bg-white bg-opacity-90 rounded-xl p-8 mt-5 '>
+                <div className='w-1/2 lg:h-[600px] mx-auto bg-white bg-opacity-90 rounded-xl p-8 mt-5 '>
                     <form onSubmit={handleFormSubmission} className='flex flex-col gap-4'>
                         <div className='flex flex-col gap-2'>
                             <label htmlFor="from" className='text-sm text-gray-500 font-semibold'>
@@ -171,6 +271,11 @@ const PlanJourney = () => {
                         <button type='submit' className='bg-blue-500 text-white px-8 py-3 rounded-full hover:bg-blue-700'>
                             Plan Journey
                         </button>
+
+                        <button type='submit' className='bg-blue-500 text-white px-8 py-3 rounded-full hover:bg-blue-700'>
+                            Save
+                        </button>
+
                     </form>
                 </div>
                 
@@ -188,31 +293,68 @@ const PlanJourney = () => {
                                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                             />
+                           
                             <Marker position={markerPosition}>
                                 <Popup>
                                 Destination: <br/>{destination}
                                 </Popup>
                             </Marker>
+
+                            <MapZoomer position={markerPosition}/>
                         </MapContainer>
                     </div>
 
                     {/* distance */}
                     <div className='text-white'>
-                        Distance: {distance} km
+                        Distance: {distance} km  <br/>
+                        State:  {state}
                     </div>
 
-                    {/* three buttons */}
-                    <div className='flex flex-1 mt-3 bg-white rounded-t-lg pt-5 px-5'>
-                        <button onClick={() => handleButton('Attractions')} className='border border-b-transparent border-black px-8 py-3 rounded-t-lg bg-slate-300 hover:bg-slate-200 hover:border-b-transparent focus:bg-slate-200'>
-                            Attractions
-                        </button>
-                        <button onClick={() => handleButton('Foods')} className='border border-b-transparent border-black px-8 py-3 rounded-t-lg bg-slate-300 hover:bg-slate-200 hover:border-b-transparent focus:bg-slate-200'>
-                            Foods
-                        </button>
-                        <button onClick={() => handleButton('Hotels')} className='border border-b-transparent border-black px-8 py-3 rounded-t-lg bg-slate-300 hover:bg-slate-200 hover:border-b-transparent focus:bg-slate-200'>
-                            Hotels
-                        </button>
-                    </div>
+                    <Menu as="div" className="relative inline-block text-left mt-3">
+                        {/* Button */}
+                        <MenuButton className="inline-flex justify-center px-4 py-2 text-sm font-medium text-black bg-slate-200 rounded-t-md hover:bg-slate-300">
+                            {currentButton}
+                        </MenuButton>
+
+                        {/* Dropdown Menu */}
+                        <MenuItems
+                            className="absolute top-0 left-full ml-2 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                            {/* Dropdown Items */}
+                            <MenuItem>
+                            {({ active }) => (<a 
+                                onClick={() => handleButton('Attractions')}
+                                className={`block px-4 py-2 text-sm ${
+                                    active ? "bg-blue-100 text-blue-700" : "text-gray-700"
+                                }`}
+                                >
+                                Attractions
+                                </a>
+                            )}
+                            </MenuItem>
+                            <MenuItem>
+                            {({ active }) => (<a
+                                onClick={() => handleButton('Foods')}
+                                className={`block px-4 py-2 text-sm ${
+                                    active ? "bg-blue-100 text-blue-700" : "text-gray-700"
+                                }`}
+                                >
+                                Foods
+                                </a>
+                            )}
+                            </MenuItem>
+                            <MenuItem>
+                            {({ active }) => (<a
+                                onClick={() => handleButton('Hotels')}
+                                className={`block px-4 py-2 text-sm ${
+                                    active ? "bg-blue-100 text-blue-700" : "text-gray-700"
+                                }`}
+                                >
+                                Hotels
+                                </a>
+                            )}
+                            </MenuItem>
+                        </MenuItems>
+                        </Menu>
                     
                     {/* content based on selected button */}
                     <div className='flex flex-1 bg-slate-200 rounded-b-lg p-5 '>
